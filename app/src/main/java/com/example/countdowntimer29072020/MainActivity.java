4ea0580c833f0d58d714d5efe1282ba7b5fdf14e
nguyenmaiphuong <68426156+phuongnguyenmai@users.noreply.github.com>
2020-08-03 18:14:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/countdowntimer29072020/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.countdowntimer29072020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView mImg;
+    int[] mArrayImages = {
+            R.drawable.hinh1,
+            R.drawable.hinh2,
+            R.drawable.hinh3,
+            R.drawable.hinh4,
+            R.drawable.hinh5
+    };
+    int mCount = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImg = findViewById(R.id.imageview);
+        CountDownTimer countDownTimer = new CountDownTimer(5100,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mImg.setImageResource(mArrayImages[mCount++]);
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
+        countDownTimer.start();
+
+
+    }
+}
